105a985304ecd0d204a92bd4c0e457198dd20174
Moslm Shlaka <mike7olwo@gmail.com>
2026-01-04 15:12:59 +0300

    fixed navigation bar icons

--- app/src/main/java/com/example/gmwrokouttimer/presentation/NavigationBar.kt
+++ app/src/main/java/com/example/gmwrokouttimer/presentation/NavigationBar.kt
@@ -1,6 +1,7 @@
 package com.example.gmwrokouttimer.presentation
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -13,8 +14,11 @@ import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Place
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,9 +28,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.gmwrokouttimer.R
 import com.example.gmwrokouttimer.navigation.Screen
 import com.example.gmwrokouttimer.presentation.screens.ProfileScreen
 import com.example.gmwrokouttimer.presentation.screens.SearchScreen
@@ -36,7 +43,7 @@ data class NavItemState(
     val route: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
-    )
+)
 
 // # Bottom Navigation Bar
 @Composable
@@ -44,7 +51,7 @@ fun NavigationBar(
     appViewModel: AppViewModel,
     noteViewModel: NoteViewModel,
 ) {
-    val items = listOf(
+    val navBarItems = listOf(
         NavItemState(
             "Main",
             "main_screen",
@@ -76,8 +83,12 @@ fun NavigationBar(
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
-            NavigationBar {
-                items.forEachIndexed { index, item ->
+            NavigationBar(
+                modifier = Modifier.fillMaxWidth(),
+                containerColor = MaterialTheme.colorScheme.background,
+                tonalElevation = 10.dp
+            ) {
+                navBarItems.forEachIndexed { index, item ->
                     NavigationBarItem(
                         selected = navBarState == index,
                         onClick = {
@@ -85,15 +96,20 @@ fun NavigationBar(
                             navController.navigate(item.route)
                         },
                         icon = {
-                            if (navBarState == index) {
-                                item.selectedIcon
-                            } else {
-                                item.unselectedIcon
-                            }
+                            Icon(
+                                imageVector = if (navBarState == index) {
+                                    item.selectedIcon
+                                } else item.unselectedIcon,
+                                contentDescription = item.title,
+                            )
                         },
-                        label = { Text(item.title) }
+                        label = { Text(item.title) },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.primary,
+                            selectedTextColor = MaterialTheme.colorScheme.primary,
+                            indicatorColor = MaterialTheme.colorScheme.background
+                        ),
                     )
-
                 }
             }
         }) { innerPadding ->
