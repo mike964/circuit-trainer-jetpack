abf2abb59ac32cacb378025ebfde01d62d26d962
Moslm Shlaka <mike7olwo@gmail.com>
2026-04-12 04:03:48 +0300

    minor fix

--- app/src/main/java/com/example/gmwrokouttimer/presentation/NoteScreen.kt
+++ app/src/main/java/com/example/gmwrokouttimer/presentation/NoteScreen.kt
@@ -1,16 +1,19 @@
 package com.example.gmwrokouttimer.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -22,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -150,7 +154,15 @@ fun WorkoutPresetItem(
                 }
             }
             Column(Modifier.weight(1f)) {
-                GifImageLocal(getExerciseById(preset.exerciseIdList[0]).imageId)
+                Box(
+                    modifier = Modifier
+                        .height(90.dp)
+//                                                .width(100.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(color = Color.LightGray)
+                ) {
+                    GifImageLocal(getExerciseById(preset.exerciseIdList[0]).imageId)
+                }
             }
 
         }
