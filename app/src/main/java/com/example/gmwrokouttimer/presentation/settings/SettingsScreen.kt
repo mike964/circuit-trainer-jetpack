11d89c14e9fd2bf004a6b72d326c14adb503e13b
Moslm Shlaka <mike7olwo@gmail.com>
2026-04-19 23:06:53 +0300

    settings screen header fixed

--- app/src/main/java/com/example/gmwrokouttimer/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/gmwrokouttimer/presentation/settings/SettingsScreen.kt
@@ -1,32 +1,57 @@
 package com.example.gmwrokouttimer.presentation.settings
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun SettingsScreen(navController: NavController) {
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Profile Screen")
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = {
-            // Go back to the previous screen (Home)
-            navController.popBackStack()
-        }) {
-            Text("Go Back")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "< Back",
+                    color = Color.Blue,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .clickable {
+                            // Go back to the previous screen (Home)
+                            navController.popBackStack()
+                        }
+                )
+            }
+            Column(Modifier.weight(3f) ,   horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Settings" ,  fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color.Gray)
+            }
+            Column(Modifier.weight(1f)) { }
         }
+        HorizontalDivider(thickness = 0.5.dp, color = Color.Gray)
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
